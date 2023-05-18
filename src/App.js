e61348310e6c7e0a1f5f839a07888a3144155656
arafatdayan005 <arafatdayan005@gmail.com>
2023-05-18 23:08:03 +0600

    React router setup added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1 className="bg-red-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Outlet></Outlet>
   );
 }
 
