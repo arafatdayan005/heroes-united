e74edd8116e03e6d284f16e9cb11901484e07be4
arafatdayan005 <arafatdayan005@gmail.com>
2023-05-30 00:04:33 +0600

    delete item with sweetaleart added

--- src/components/MyToys.js
+++ src/components/MyToys.js
@@ -1,15 +1,49 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../providers/AuthProvider'
+import Swal from 'sweetalert2';
 
 function MyToys() {
     const { user } = useContext(AuthContext)
     const [data, setData] = useState([])
+    const [render, setRender] = useState(false)
 
     useEffect(() => {
         fetch(`https://heroes-united-server-arafatdayan005.vercel.app/products/${user?.email}`)
             .then(res => res.json())
             .then(p => setData(p))
-    }, [user])
+    }, [user, render])
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`https://heroes-united-server-arafatdayan005.vercel.app/products/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                })
+                    .then(res => res.json())
+                    .then(r => {
+                        if (r.deletedCount == 1) {
+                            setRender(!render)
+                        }
+                    })
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    }
 
     return (
         <>
@@ -17,10 +51,10 @@ function MyToys() {
             <div className='max-w-[85%] mx-auto'>
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg mb-16">
                     <div className="pb-4 bg-white dark:bg-gray-900">
-                        <label for="table-search" className="sr-only">Search</label>
+                        <label htmlFor="table-search" className="sr-only">Search</label>
                         <div className="relative mt-1">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                             </div>
                             <input type="text" id="table-search" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
                         </div>
@@ -79,7 +113,7 @@ function MyToys() {
                                             {d.quantity}
                                         </td>
                                         <td className="px-6 py-4">
-                                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center">Delete</button>
+                                            <button onClick={() => handleDelete(d._id)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center">Delete</button>
                                         </td>
                                         <td className="px-6 py-4">
                                             <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center">Update</button>
